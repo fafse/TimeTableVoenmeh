c93d469e84e45b3e7a80ea649aa988ed55fc9dc9
fafse <bacyabro@gmail.com>
2023-06-14 11:12:56 +0300

    Fixed keyboard bug

--- app/src/main/java/com/example/timetablevoenmeh/SettingsFragment.java
+++ app/src/main/java/com/example/timetablevoenmeh/SettingsFragment.java
@@ -7,84 +7,74 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.timetablevoenmeh.TimeTableHandler.TimeTable.DateFormatter;
 import com.example.timetablevoenmeh.TimeTableHandler.TimeTable.TimeTableHandler;
 
-import org.w3c.dom.Text;
-
 import java.io.IOException;
-import java.io.PrintStream;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SettingsFragment extends Fragment {
 
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    private TextView lessonNotFoundTextView;
     private TimeTableHandler timeTableHandler;
     private DateFormatter dateFormatter;
-    private TextView groupNameTextView;
+    private EditText groupNameTextView;
     private String TAG = "SETTINGSFRAGMENT";
+    private TextView ErrorChangeGroupTextView;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         groupNameAcceptButton = getView().findViewById(R.id.groupNameInputButton);
-        groupNameTextView=getView().findViewById(R.id.groupNameInput);
+        groupNameTextView = (EditText) getView().findViewById(R.id.groupNameInput);
+        ErrorChangeGroupTextView = getView().findViewById(R.id.ErrorChangeGroupTextView);
     }
 
     private Button groupNameAcceptButton;
+
     public SettingsFragment() {
         // Required empty public constructor
     }
 
-    public interface PassMeLinkObject{
+    public interface PassMeLinkObject {
 
     }
 
     @Override
     public void onStart() {
         super.onStart();
+        groupNameTextView.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    new Thread(new Runnable() {
+                        public void run() {
+                            handleGroupChange();
+                        }
+                    }).start();
+                    return true;
+                }
+                return false;
+            }
+        });
         groupNameAcceptButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new Thread(new Runnable() {
                     public void run() {
-                        try {
-                            Log.i(TAG, "run: "+timeTableHandler.getGroupName());
-                            if (timeTableHandler.setGroupName(groupNameTextView.getText().toString())) {
-                                getActivity().runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        groupNameTextView.setText("");
-                                    }
-                                });
-                            }
-                        } catch (IOException e) {
-
-                            getActivity().runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    Toast toast = Toast.makeText(getActivity(),
-                                            "Ресурс недоступен. Проверьте соединение с интернетом", Toast.LENGTH_LONG);
-                                    toast.show();
-                                }
-                            });
-                        }
+                        handleGroupChange();
                     }
                 }).start();
 
@@ -93,20 +83,56 @@ public class SettingsFragment extends Fragment {
         });
     }
 
+    private void handleGroupChange() {
+        try {
+            Log.i(TAG, "run: " + timeTableHandler.getGroupName());
+            if (timeTableHandler.setGroupName(groupNameTextView.getText().toString().replaceAll("\n",""))) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getActivity(), "Группа изменена на " + timeTableHandler.getGroupName(), Toast.LENGTH_SHORT).show();
+                        groupNameTextView.setText("");
+                    }
+                });
+            } else {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ErrorChangeGroupTextView.setVisibility(View.VISIBLE);
+                        ErrorChangeGroupTextView.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                ErrorChangeGroupTextView.setVisibility(View.GONE);
+
+                            }
+                        }, 3000);
+                    }
+                });
+
+            }
+        } catch (IOException e) {
+
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast toast = Toast.makeText(getActivity(),
+                            "Ресурс недоступен. Проверьте соединение с интернетом", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            });
+        }
+    }
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Bundle bundle= this.getArguments();
-        if(bundle!=null)
-        {
-            timeTableHandler=(TimeTableHandler) bundle.getSerializable("TIMETABLEHANDLER");
-            dateFormatter=(DateFormatter) bundle.getSerializable("DATAFORMATTER");
-            Log.i(TAG, "onCreate: "+dateFormatter.getCurrentDate());
-        }
-        else
-        {
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            timeTableHandler = (TimeTableHandler) bundle.getSerializable("TIMETABLEHANDLER");
+            dateFormatter = (DateFormatter) bundle.getSerializable("DATAFORMATTER");
+            Log.i(TAG, "onCreate: " + dateFormatter.getCurrentDate());
+        } else {
             Log.i(TAG, "onCreate: bundle NULL");
         }
     }
@@ -115,6 +141,7 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 
